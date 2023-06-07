6c2a05c19f22da73c1cb678c008168c6046b7fe6
Mathieu Walravens <wama@odoo.com>
2023-06-07 21:52:02 +0200

    Create script to generate Pokeclicker types

---
+++ typegen.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/sh
+
+ROOT_DIR="$(realpath $(dirname "$0"))"
+POKECLICKER_DIR="$(realpath "$(dirname "$0")/../pokeclicker")"
+TYPE_OUT_DIR="${ROOT_DIR}/pokeclicker"
+mkdir -p $TYPE_OUT_DIR
+
+cd $POKECLICKER_DIR
+
+echo "build Pokecliker modules"
+npm run clean && npm start
+
+echo "export Pokecliker declarations"
+npx tsc -d --emitDeclarationOnly --outDir ./out
+
+echo "Copy declaration files"
+rsync -a "${POKECLICKER_DIR}/src/declarations/" "${TYPE_OUT_DIR}"
+rsync -a "${POKECLICKER_DIR}/out/" "${TYPE_OUT_DIR}"
+
